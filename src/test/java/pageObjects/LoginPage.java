84a9b1696013d1472fd80f6f043fbc9280cecdff
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-19 19:08:44 +0100

    LoginPage class with locators and methods

---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,45 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+     WebDriver driver;
+
+
+    @FindBy(xpath = "//span[text()='My Account']")
+    private WebElement myAccount;
+
+    @FindBy(xpath = "//a[text()='Login']")
+    private WebElement login;
+
+    @FindBy(id = "input-password")
+    private WebElement setPassword;
+
+    @FindBy(id = "input-email")
+    private WebElement setEmail;
+
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
+    }
+
+    public void getMyAccount() {
+        myAccount.click();
+    }
+
+    public void getLogin() {
+        login.click();
+    }
+
+    public void setUserEmail(String email) {
+        setEmail.sendKeys(email);
+    }
+    public void setUserPassword(String password) {
+        setEmail.sendKeys(password);
+    }
+}
