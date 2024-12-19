68f02fd1056a54f31a64b475c24fbd5149d3a239
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-19 19:13:06 +0100

    TestRun class for running glutting steps in to features. Report plugin after tests

---
+++ src/test/java/testRunner/TestRun.java
@@ -0,0 +1,17 @@
+package testRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features = ".//Feature//Login.feature",
+                   glue = "stepsDefinitions",
+                    dryRun = false,
+                    plugin = {"pretty","html:Patrick`s report"})
+
+public class TestRun {
+
+}
