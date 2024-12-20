ba82c46d68d2fa364db4f935dbfbce77afa6e9eb
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-20 01:39:48 +0100

    Changed reports directory

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -6,11 +6,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions( features = ".//Feature//Login.feature",
                    glue = "stepsDefinitions",
                     dryRun = false,
-                    plugin = {"pretty","html:Patrick`s report"})
+                    plugin = {"pretty","html:.//src//test//resources//reports//Patrick`s report"})
 
 public class TestRun {
 
