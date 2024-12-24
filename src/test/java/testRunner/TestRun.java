2160daa59879785c4788ca5a26e2e9f52879d99f
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-24 02:00:35 +0100

    Added one more feature and changed report path directory. Defined report as HTML

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -8,10 +8,11 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = ".//Feature//Login.feature",
-                   glue = "stepsDefinitions",
+@CucumberOptions(
+        features = {".//Feature//Register.feature",".//Feature//Login.feature"},
+                   glue = {"stepsDefinitions","hooks"},
                     dryRun = false,
-                    plugin = {"pretty","html:.//src//test//resources//reports//Patrick`s report"})
+                    plugin = {"pretty","html:.//src//test//resources//reports//Patrick`s report.html"})
 
 public class TestRun {
 
