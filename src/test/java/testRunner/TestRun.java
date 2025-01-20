bbf033148fd368e99f34ef575f0d376d29256f7a
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-20 22:20:00 +0100

    Feature run

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {".//Feature//Register.feature",".//Feature//Login.feature"},
+        features = {".//Feature"},
                    glue = {"stepsDefinitions","hooks"},
                     dryRun = false,
                     plugin = {"pretty","html:.//src//test//resources//reports//Patrick`s report.html"})
